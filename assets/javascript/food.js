76d16150b8005222a9f341acb04245bf6ef05080
Muktiar Idrees <imuktiar@hotmail.com>
2019-01-22 21:37:38 -0500

    update2

--- assets/javascript/food.js
+++ assets/javascript/food.js
@@ -37,6 +37,7 @@ $(document).ready(function () {
         let recipeImage = $('<img>');
         $(recipeImage).attr('src', image)
 
+
         $(recipeDiv).append(recipeImage)
 
         $("#food-row").append(recipeDiv)

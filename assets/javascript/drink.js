1f5d467e50897b4e8eb4e7637d9a8330563f7324
ranton <ranton@Dads-Retina-MacBookPro.local>
2019-01-24 02:40:32 -0500

    js, css changes

--- assets/javascript/drink.js
+++ assets/javascript/drink.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
 
         let userInput = $('#drink-input').val().trim();
 
-        // Cocktail API with no ID/key required assignment with E6 template string for query
-        // let queryURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`
+        //     // Cocktail API with no ID/key required assignment with E6 template string for query
+        //     // let queryURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`
 
         let queryURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${userInput}`
 
@@ -20,16 +20,17 @@ $(document).ready(function () {
             // console.log(response.drinks);
 
             for (let i = 0; i < response.drinks.length; i++) {
-                // console.log(response.drinks[i].strDrink, response.drinks[i].strDrinkThumb);
-                createDrinkRow(response.drinks[i].strDrink, response.drinks[i].strDrinkThumb);
+                createDrinkRow(response.drinks[i].strDrink, response.drinks[i].strDrinkThumb, i);
             }
         });
     });
 
-    createDrinkRow = (name, image) => {
+    createDrinkRow = (name, image, id) => {
         let drinkDiv = $('<div>');
         let thumb = $('<img>');
         let button = $('<button>');
+
+        $(drinkDiv).attr('id', id)
 
         $(thumb).attr('src', image);
         $(button).attr('id', 'details');
@@ -39,6 +40,9 @@ $(document).ready(function () {
         $(drinkDiv).append(name);
         $(drinkDiv).append(button);
 
+
+
+
         $('#drink-row').append(drinkDiv);
 
     }
@@ -46,6 +50,9 @@ $(document).ready(function () {
     $(document).on('click', '#details', function () {
 
         let detailName = $(this).attr('name');
+        let newDiv = $('<div>');
+        $(this).append(newDiv)
+        console.log('The name of this ' + 'drink is: ' + detailName);
 
         let detailDiv = $('<div>');
         $(this).append(detailDiv);
@@ -56,32 +63,64 @@ $(document).ready(function () {
             url: queryURL,
             method: 'GET'
         }).then(function (response) {
+            for (let i = 0; i < response.drinks.length; i++) {
+                console.log(response);
+                console.log('full array: ' + response.drinks[0].strIngredient1);
+                console.log('loop through instructions to find this: ' + response.drinks[i].strInstructions);
+                console.log(response.drinks[0].strIngredient10);
+                // let str = response.drinks[0].strIngredient10
+                // str = str.substring(0, str.length -2);
+                // console.log(str);
 
-            console.log(response.drinks)
+                $(newDiv).append(response.drinks[i].strInstructions)
+                console.log(response)
 
-            let strIngredients = 'response.drinks[0].strIngredient';
-            let strMeasure = "response.drinks[0].strMeasure";
-            let arrIngredients = [];
+                for (let i = 1; i < 16; i++) {
+                    let strIngredients = 'response.drinks[0].strIngredient';
+                    // let strMeasure = 'response.drinks[0].strMeasure';
+                    let arrIngredients = [];
+                    let arrMeasures = [];
+                    let ingredient = strIngredient + i;
+                    arrIngredients.push(ingredient[i]);
+                    // let measure = strMeasure + i;
+                    // arrMeasure.push(measure[i]);
 
+                    console.log(strIngredients);
+                    console.log(arrIngredients);
+                    // console.log(strMeasure);
+
+                };
+            }
+        })
+    })
+    let arrIngredients = [];
+    getIngredients = () => {
+        for (let i = 1; i < 16; i++) {
+            let strIngredient = str + i
+            arrIngredients.push(strIngredient);
+        }
+        let arrMeasures = [];
+        getMeasures = () => {
             for (let i = 1; i < 16; i++) {
-                let ingredient = str + i;
-
-                arr.push(ingredient);
-
-                //$(detailDiv).append(eval(ingredient));
-                
+                let strMeasure = str + i
+                arrMeasures.push(strMeasures);
             }
 
-            console.log(arr)
-            
 
-        });
+            $(detailDiv).append(eval(ingredient));
 
-    });
-
-});
+        }
+    }
+})
 
 
+/*        let detailsDiv = $('<div>');
+
+        $(detailsDiv).attr(this);
+
+        $(detailsDiv).append(this);
+
+        $('#details-div').append(detailsDiv);*/
 
 //------------------------------------------------------------------------
 // Drink Image:
